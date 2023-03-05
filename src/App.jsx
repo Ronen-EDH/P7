224d397a6553e0b507169281b154b7c43052a55d
David Horvath <calogero.c.23@gmail.com>
2023-03-05 18:00:45 +0100

    Post button works with user input now

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Posts } from "./components/Posts";
 import CreatePost from "./components/CreatePost";
 import { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,14 +12,29 @@ function App() {
     },
   ]);
 
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  // <input type="text" onChange={(e) => setTitle(e.target.value)}>
+  // <input type="text" onChange={(e) => setText(e.target.value)}>
+  const newTitle = () => {
+    return <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)} />;
+  };
+
+  // console.log("title:", title);
+
+  const newText = () => {
+    return <Form.Control as="textarea" rows={3} value={text} onChange={(e) => setText(e.target.value)} />;
+  };
+
   const addNewPost = () => {
-    console.log("I have been clicked");
-    return setPosts((prevPosts) => [...prevPosts, { title: "Whatever", text: "Whatever" }]);
+    // console.log("I have been clicked");
+    return setPosts((prevPosts) => [...prevPosts, { title: title, text: text }]);
   };
 
   return (
     <div>
-      <CreatePost func={addNewPost} />
+      <CreatePost funcNewPost={addNewPost} funcNewTitle={newTitle()} funcNewText={newText()} postDisabled={title == "" || text == ""} />
       <Posts posts={posts} />
     </div>
   );
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -7,14 +7,16 @@ export default function CreatePost(props) {
     <Form>
       <Form.Group className="mb-3" controlId="post__Title">
         <Form.Label>Title</Form.Label>
-        <Form.Control type="text" />
+        {/* <Form.Control type="text" value={props.value} onChange={(e) => setNewItem(e.target.value)} /> */}
+        {props.funcNewTitle}
       </Form.Group>
       <Form.Group className="mb-3" controlId="post__Textarea1">
         <Form.Label>Text</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        {/* <Form.Control as="textarea" rows={3} /> */}
+        {props.funcNewText}
       </Form.Group>
       <StyledWrap>
-        <Button onClick={props.func} variant="primary">
+        <Button onClick={props.funcNewPost} disabled={props.postDisabled} variant="primary">
           Post
         </Button>{" "}
       </StyledWrap>
