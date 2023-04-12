1425ca1a9140178f8543e9077029b7108db0df8c
David Horvath <calogero.c.23@gmail.com>
2023-04-12 18:55:59 +0200

    Minor CSS change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,9 @@ export function Header() {
   function renderPath() {
     let currentLocation = location.pathname;
     if (currentLocation === "/posts") {
-      return <h1>Create a post</h1>;
+      return <h1 className="fs-2">Create a post</h1>;
     } else if (currentLocation === "/profile") {
-      return <h1>Profile page</h1>;
+      return <h1 className="fs-2">Profile page</h1>;
     }
   }
 
